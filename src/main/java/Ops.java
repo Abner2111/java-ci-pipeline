b451394de5b05f0624ede0aaf91aad7b1bff1a5b
Abner2111 <38987646+Abner2111@users.noreply.github.com>
2021-09-08 22:57:58 -0600

    Initial commit

---
+++ src/main/java/Ops.java
@@ -0,0 +1,17 @@
+public class Ops{
+    public  float add(float num1, float num2){
+        return num1 + num2;
+    }
+
+    public  float substract(float num1, float num2){
+        return num1 - num2;
+    }
+
+    public float divide(float num1, float num2){
+        return num1/num2;
+    }
+
+    public float product(float num1, float num2){
+        return num1*num2;
+    }
+}
---
+++ src/test/java/OpsTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OpsTest {
+    @Test
+    public void testSuma(){
+        Ops operaciones = new Ops();
+        assertEquals(2, operaciones.add(1,1));
+    }
+
+    @Test
+    public void testProducto(){
+        Ops op= new Ops();
+        assertEquals(9, op.product(4.5F,2));
+    }
+}
